7fb320d9e14735991c949cc6596be8a4f2d45c2f
ngusev <ngusev375@gmail.com>
2025-12-22 17:48:11 +0300

    Создал ERD-диаграмму и файлы для создания и заполнения БД.

---
+++ src/main/java/com/task_11_1/insert_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO clients VALUES
+(1, 'Ivanov Ivan Ivanovich', 25);
+
+INSERT INTO orders VALUES
+(1, 1, 0, 449.99, '2025-11-21 10:20:00', 1);
+
+INSERT INTO books VALUES
+(1, 'Little prince', 'A. D. Exupery', ' ', '2005-11-21 10:20:00', true, 5, 449.99);
+
+INSERT INTO orders_books VALUES
+(1, 1);
+
+INSERT INTO requests VALUES
+(1, 1, 2, true);
