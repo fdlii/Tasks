cf139cf7e2239424291c9af17b7f53167a334233
ngusev <ngusev375@gmail.com>
2026-01-31 16:45:58 +0300

    Сделал HelloWorld сервлет.

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,14 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "helloServlet", value = "/hello")
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Hello world");
+    }
+}
