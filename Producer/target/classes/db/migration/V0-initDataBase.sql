efd812eb9f90823e748e643a32f3e76fcf662c62
ngusev <ngusev375@gmail.com>
2026-02-14 23:05:15 +0300

    Создал БД с таблицами, настроил Spring Boot.

---
+++ Producer/target/classes/db/migration/V0-initDataBase.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS accounts (
+    id INT PRIMARY KEY,
+    money NUMERIC
+);
+
+CREATE TABLE IF NOT EXISTS transfers (
+    id INT PRIMARY KEY,
+    withdrawal_account_id INT,
+    transfer_account_id INT,
+    summ NUMERIC,
+    status INT,
+
+    CONSTRAINT fk_withdrawal
+    FOREIGN KEY (withdrawal_account_id)
+    REFERENCES accounts(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
